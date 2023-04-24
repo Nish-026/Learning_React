1a03824f7198dffd664f69c3872ee2e25e2a92dd
Nish-026 <nishtha.kashyap2608@gmail.com>
2023-04-24 20:07:55 +0530

    React First practice

---
+++ First_project/index.js
@@ -0,0 +1,5 @@
+ReactDOM.render(<h1>Hello,everyone!</h1>, document.getElementById("root"))
+
+//ReactDOM is a global variable which has a render method that helps in adding the elements
+// so first i pass the value that i want on the display, second is where i want it
+
